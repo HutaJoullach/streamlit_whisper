3cbfecf9114f495d7c903c859a1eea2acec4d02d
HutaJoullach <hutajoullach@gmail.com>
2023-02-27 10:31:16 +0900

    refactored code

--- main.py
+++ main.py
@@ -5,21 +5,8 @@ st.title('Whisper Transcription App')
 
 audio_file = st.file_uploader('Upload Audio', type=['wav', 'mp3', 'm4a'])
 
-# @st.cache
-# def load_whisper_media():
-#     model = whisper.load_model('base')
-#     return model
-
-# if st.sidebar.button('Load Whisper Model'):
-#     model = load_whisper_media()
-#     st.sidebar.success('Whisper Model Loaded')
-
 model = whisper.load_model('base')
 st.text('Whisper Model Loaded')
-
-# def get_audio_file_details(file):
-#     file_details = {'FileName': file.name, 'FileType': file.type, 'FileSize': file.size}
-#     return file_details
 
 if st.sidebar.button('Transcribe Audio'):
     if audio_file is not None:
